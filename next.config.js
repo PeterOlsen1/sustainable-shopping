647a991640b886adcdd1241173d74e2c5d577776
Peter Olsen <peter.olsen@thrivent.com>
2025-07-22 09:41:01 -0500

    update config to include all hosts

--- next.config.js
+++ next.config.js
@@ -4,26 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "platform-lookaside.fbsbx.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        port: "",
-        pathname: "/**",
-      },
-
-      {
-        protocol: "https",
-        hostname: "s3.amazonaws.com",
+        hostname: "*",
         port: "",
         pathname: "/**",
       },
