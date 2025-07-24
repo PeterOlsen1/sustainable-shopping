4d549ca7f5acefdc2bdcf1027655f761e165da68
Peter Olsen <peter.olsen@thrivent.com>
2025-07-24 11:47:48 -0500

    run formatting

--- actions/head/setHead.ts
+++ actions/head/setHead.ts
@@ -2,12 +2,12 @@ export default function setHead(title: string, description: string) {
   if (typeof window !== "undefined") {
     document.title = title;
 
-  let metaDescription = document.querySelector('meta[name="description"]');
-  if (!metaDescription) {
-    metaDescription = document.createElement("meta");
-    metaDescription.setAttribute("name", "description");
-    document.head.appendChild(metaDescription);
-  }
+    let metaDescription = document.querySelector('meta[name="description"]');
+    if (!metaDescription) {
+      metaDescription = document.createElement("meta");
+      metaDescription.setAttribute("name", "description");
+      document.head.appendChild(metaDescription);
+    }
     metaDescription.setAttribute("content", description);
   }
 }
